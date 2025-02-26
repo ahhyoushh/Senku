25d4c46f2ea463c2211b303a501324e3fd7eacd7
ahhyoushh <bhaleraoayush06@gmail.com>
2025-02-26 13:54:41 +0530

    Removed credentials and updated

--- E-connect_back/firebase_init.py
+++ E-connect_back/firebase_init.py
@@ -1,5 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, auth
+import os
+import json
+import base64
+
+creds_base64 = os.getenv("GOOGLE_CLOUD_CREDENTIALS")
+
+creds_json = json.loads(base64.b64decode(creds_base64))
+
+with open("credentials.json", "w") as f:
+    json.dump(creds_json, f)
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred)
