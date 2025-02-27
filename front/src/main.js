c3694bd2b598be962fcef0882e7451c897055e6e
ahhyoushh <bhaleraoayush06@gmail.com>
2025-02-27 19:59:35 +0530

    Added pwa support

--- front/src/main.js
+++ front/src/main.js
@@ -6,6 +6,22 @@ import 'vue-toastification/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './registerServiceWorker'
+import { register } from 'register-service-worker';
+
+if (process.env.NODE_ENV === 'production') {
+  register('/service-worker.js', {
+    ready() {
+      console.log('PWA is ready.');
+    },
+    registered() {
+      console.log('Service worker registered.');
+    },
+    updated() {
+      console.log('New content available, refresh needed.');
+    }
+  });
+}
 
 const app = createApp(App)
 
